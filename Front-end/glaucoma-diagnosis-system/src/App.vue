c3b2c9a1b26402c969ad7c2cb26f8319d5a2f008
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2021-07-27 20:06:53 +0300

    Revert "[web] Configure router"

--- Front-end/glaucoma-diagnosis-system/src/App.vue
+++ Front-end/glaucoma-diagnosis-system/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <router-view/>
+  <div class="flex justify-center">
+      <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    
+    HelloWorld
   }
 })
 </script>
