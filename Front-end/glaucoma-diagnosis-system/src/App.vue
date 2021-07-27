b5c288f1c98cea428121d07759d9c0007cf0a504
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2021-07-27 20:13:04 +0300

    Revert "Revert "[web] Configure router" (#3)"
    This reverts commit c3ff53572081912ce0d6b8bf3a3eb6a08047b455.

--- Front-end/glaucoma-diagnosis-system/src/App.vue
+++ Front-end/glaucoma-diagnosis-system/src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="flex justify-center">
-      <img alt="Vue logo" src="./assets/logo.png" />
-  </div>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <router-view/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 })
 </script>
---
+++ Front-end/glaucoma-diagnosis-system/src/views/Register.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Register</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Register',
+})
+</script>
