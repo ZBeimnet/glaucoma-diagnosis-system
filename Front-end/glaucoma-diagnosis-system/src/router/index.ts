9e11ad6f318e1787fa4d6c4a5c280f0573fe0332
BekyH <bereketheramo25@gmail.com>
2021-08-01 16:16:19 +0300

    admin manage page

--- Front-end/glaucoma-diagnosis-system/src/router/index.ts
+++ Front-end/glaucoma-diagnosis-system/src/router/index.ts
@@ -3,6 +3,7 @@ import Home from '/src/views/Home.vue'
 import Login from '/src/views/Login.vue'
 import Register from '/src/views/Register.vue'
 import Reception from '/src/views/Reception.vue'
+import adminmanage from '/src/views/adminManage.vue';
 
 const routes = [
     {
@@ -21,12 +22,18 @@ const routes = [
         component: Register,
     },
     {
+
         path: '/reception',
         name: 'Reception',
         component: Reception,
+    },
+    {
+        path:'/adminmanage',
+        name:'Adminmanage',
+        component:adminmanage,
     }
-
 ]
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
---
+++ Front-end/glaucoma-diagnosis-system/src/views/adminManage.vue
@@ -0,0 +1,117 @@
+<template>
+        <div>
+            <div class="flex mt-3 ml-3">
+                <ul class="flex border-b">
+  <li class="-mb-px mr-1">
+    <a class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="#">Approved</a>
+  </li>
+  <li class="mr-1">
+    <a class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">requested</a>
+  </li>
+  
+</ul>
+            </div>
+        
+  <div class="flex flex-col mx-5 my-5">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Name
+                </th>
+                
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Region
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  City
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Medical License
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Actions
+                </th>
+                
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="person in people" :key="person.email">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="flex-shrink-0 h-10 w-10">
+                      <img class="h-10 w-10 rounded-full" :src="person.image" alt="" />
+                    </div>
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ person.name }}
+                      </div>
+                      <div class="text-sm text-gray-500">
+                        {{ person.email }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">{{ person.title }}</div>
+                  <div class="text-sm text-gray-500">{{ person.department }}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    Active
+                  </span>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ person.role }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                   <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-3">Approve</button>
+                  <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Deny</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+const people = [
+  {
+    name: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+   {
+    name: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  }
+]
+export default defineComponent({
+  name: "adminmanage",
+   setup() {
+    return {
+      people,
+    }
+   }
+});
+</script>
+
+<style>
+</style>
