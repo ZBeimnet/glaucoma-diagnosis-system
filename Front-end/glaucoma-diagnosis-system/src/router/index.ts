b249955ad343a01a755c26ebed3a9ca26bee7ec2
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2021-08-03 15:25:17 +0300

    [Web]lab technician page (#11)
    * lab technician page
    
    * conflict resolved

--- Front-end/glaucoma-diagnosis-system/src/router/index.ts
+++ Front-end/glaucoma-diagnosis-system/src/router/index.ts
@@ -3,8 +3,7 @@ import Home from '/src/views/Home.vue'
 import Login from '/src/views/Login.vue'
 import Register from '/src/views/Register.vue'
 import Reception from '/src/views/Reception.vue'
-import adminmanage from '/src/views/adminManage.vue';
-
+import Labratory from '/src/views/Labratory.vue'
 const routes = [
     {
         path: '/',
@@ -28,9 +27,9 @@ const routes = [
         component: Reception,
     },
     {
-        path:'/admin/manage',
-        name:'Adminmanage',
-        component:adminmanage,
+        path: '/lab',
+        name: 'Labratory',
+        component: Labratory,
     }
 ]
 
