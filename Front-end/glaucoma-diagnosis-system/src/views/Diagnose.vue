932c29204ebd85b6bdaad0caf6b04566e826ed46
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2021-08-04 16:21:41 +0300

    [Web] changes in diagnose page (#13)
    * changes in diagnose page
    
    * files tab removed

--- Front-end/glaucoma-diagnosis-system/src/views/Diagnose.vue
+++ Front-end/glaucoma-diagnosis-system/src/views/Diagnose.vue
@@ -1,82 +1,126 @@
 <template>
-    <div class="w-full h-full mt-0 font-sans antialiased bg-grey-lighter ">
-        <div class="w-full bg-grey-lightest mt-0 " style="padding-top: 4rem;">
-            <div class="container mx-auto py-8">
-                <div v-for = "patient in patients" :key="patient.card_number">
-                    <div class="w-5/6 lg:w-1/2 mx-auto bg-white rounded shadow ">
-                        <div class="py-4 px-8 content-center">
-                            <div class="flex mb-4 justify-center">
-                                <div class="w-1/2 mr-1">
-                                    <label class="text-grey-darker text-sm font-bold mb-2" for="card_number">Card Number</label>
-                                    <p class="float-right" id="card_number"> {{patient.card_number}} </p>
-                                </div>
-                            </div>
-
-                            <div class="flex mb-4 justify-center">
-                                <div class="w-1/2 mr-1 ">
-                                    <label class="text-grey-darker text-sm font-bold mb-2 " for="name">Name</label>
-                                    <p class="float-right" id="name"> {{patient.name}} </p>
-                                </div>
-                            </div>
-                            <div class="flex mb-4 justify-center">
-                                <div class="w-1/2 mr-1">
-                                    <label class="text-grey-darker text-sm font-bold mb-2" for="gender">Gender</label>
-                                    <p class="float-right" id="gender"> {{patient.gender}} </p>
-                                </div>
-                            </div>
-                            <div class="flex mb-4 justify-center">
-                                <div class="w-1/2 mr-1">
-                                    <label class="text-grey-darker text-sm font-bold mb-2" for="computer_usage">Computer Usage</label>
-                                    <p class="float-right" id="computer_usage"> {{patient.computer_usage}} hour/day </p>
-                                </div>
-                            </div>
-                            <div class="flex mb-4 justify-center">
-                                <div class="w-1/2 mr-1">
-                                    <button class="w-1/3 bg-blue-500  rounded text-white mr">
-                                        Open Image
-                                    </button>
-                                    <button class="w-1/2 bg-blue-500  rounded text-white float-right">
-                                        Patient's History
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="relative text-gray-700 mt-10">
-                                <button class="bg-blue-500 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-                                    Diagnose
-                                </button>
-                                <button class="bg-blue-500 text-white font-bold py-2 px-4 border border-blue-700 rounded float-right">
-                                    Next
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+  <div class="bg-gray-100">
+    <div class="container mx-auto my-5 p-5">
+      <div class="md:flex no-wrap md:-mx-2">
+        <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+            <div class="animate-pulse flex space-x-4 hidden" id="result">
+                <div class="flex-1 space-y-4 py-1">
+                <div class="h-4 bg-blue-400 rounded w-3/4"></div>
+                <div class="space-y-2">
+                    <div class="h-4 bg-blue-400 rounded"></div>
+                    <div class="h-4 bg-blue-400 rounded w-5/6"></div>
+                </div>
                 </div>
             </div>
+            <div class="flex mt-10 hidden" id="accept">
+                <button class="w-1/2 "><i class="fa fa-check fa-2x" style="color:green;"></i></button>
+                <button><i class="fas fa-times-circle fa-2x" style="color:red;" ></i></button>
+            </div>
         </div>
+        
+        <div class="w-full md:w-9/12 mx-2 h-64">
+          <div class="bg-white p-3 shadow-sm rounded-sm">
+            <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+              <h1 class="w-1/2 mb-5 text-green-500 font-thick text-lg ml-5">Card Number <span class="float-right">{{card_number}}</span></h1>
+            </div>
+            <div class="text-gray-700">
+              <div class="grid md:grid-cols-2 text-sm">
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">First Name</div>
+                  <div class="px-4 py-2">{{first_name}}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">Last Name</div>
+                  <div class="px-4 py-2">{{last_name}}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">Gender</div>
+                  <div class="px-4 py-2">{{gender}}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">Contact No.</div>
+                  <div class="px-4 py-2">{{phone_number}}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">Region</div>
+                  <div class="px-4 py-2">{{region}}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">Subcity</div>
+                  <div class="px-4 py-2">{{sub_city}}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">Age</div>
+                  <div class="px-4 py-2">{{age}}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                  <div class="px-4 py-2 font-semibold">Image</div>
+                  <div class="px-4 py-2"><button><i class="fas fa-expand"></i></button></div>
+                </div>
+              </div>
+            </div>
+            <div class="inline-flex mt-5">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" @click="get_result">
+                    Diagnose
+                </button>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  ml-5 fas fa-arrow-right" @click="get_next_patient">
+                    
+                </button>
+            </div>
+          </div>
+
+          <div class="my-4"></div>
+          
+
+          <div class="bg-white p-3 shadow-sm rounded-sm">
+              <div class="bg-white">
+                <nav class="flex flex-col sm:flex-row">
+                    <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none text-blue-500 border-b-2 font-medium border-blue-500">
+                        Patient Info
+                    </button>
+                    <button class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none click:">
+                        Patient History
+                    </button>
+                </nav>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, reactive } from 'vue'
-    const patients = [
-        {
-            card_number: "12",
-            name: "John Doe",
+    import { defineComponent, reactive } from "vue";
+    
+
+    export default defineComponent({
+    name: "Diagnose",
+    methods :{
+        get_result() {
+            document.getElementById("result").style.display = 'block'
+            document.getElementById("accept").style.display = 'block'
+        },
+        get_next_patient(){
+            document.getElementById("result").style.display = 'None'
+            document.getElementById("accept").style.display = 'None'
+        }
+    },  
+    data() {
+        return {
+            card_number: "112",
+            first_name: "John",
+            last_name: "Doe",
             age: 42,
             gender: "M",
             computer_usage: 4,
-            image: "file://dir"
-        }
-    ];
-    export default defineComponent ({
-        name: "Diagnose",
-        methods:{
+            image: "file://dir",
+            region: "Addis Ababa",
+            sub_city: "Gullele",
+            phone_number: "0922",
 
-        },
-        setup(){
-            return {
-                patients
-            };
         }
-    })
+        
+    },
+    });
 </script>
