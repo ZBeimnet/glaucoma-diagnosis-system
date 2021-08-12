c67fab2bec7d0494151d3a9778f078db02380313
Beimnet.Z <zbeimnet@gmail.com>
2021-08-12 10:11:35 +0300

    [Back-endd] Heroku fix#2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+var port_number = process.env.PORT || 8080;
 
 app.get('/',(req,res)=>{
     res.send("Hello world");
     
 })
 
-app.listen(port,()=>{
-    console.log(`GDS is listening at http://localhost:${process.env.port || port}`);
+app.listen(port_number,()=>{
+    console.log(`GDS is listening at http://localhost:${port_number}`);
 })
