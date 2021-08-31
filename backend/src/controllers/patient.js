8fe25a2ca4e3debe7252ef7561d44bbffdf2d9d1
BekyH <bereketheramo25@gmail.com>
2021-08-31 16:59:10 +0300

    changes

--- backend/src/controllers/patient.js
+++ backend/src/controllers/patient.js
@@ -7,8 +7,9 @@ const storage = multer.diskStorage({
     destination:function(req,file,cb){
         cb(null,path.join(__dirname,"../../public/images"));
     },
-    filename:function(req,file,cb){
-        cb(null,`patient-${req.params.id}-${req.body.date}${path.extname(file.originalname)}`);
+    filename:async function(req,file,cb){
+        const Patient = await patient.findById(req.params.id);
+        cb(null,`patient-${Patient.cardNumber}-${req.body.date}${path.extname(file.originalname)}`);
     }
 })
 
