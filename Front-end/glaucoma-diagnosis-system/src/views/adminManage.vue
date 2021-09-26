27549648c73080253d296db0787d5ba53b254bc6
Beimnet Zewdu <zbeimnet@gmail.com>
2021-09-26 14:58:58 +0300

    [web] Integrate admin approve/deny (#62)

--- Front-end/glaucoma-diagnosis-system/src/views/adminManage.vue
+++ Front-end/glaucoma-diagnosis-system/src/views/adminManage.vue
@@ -17,199 +17,6 @@
                 font-semibold
               "
               href="#"
-              >Approved</a
-            >
-          </li>
-        </ul>
-      </div>
-
-      <div v-if="healthcenterLoading" class="flex flex-col justify-center items-center">
-        <Loader :loaderColor="loaderColor"/>
-      </div>
-
-      <div class="flex flex-col my-5" v-if="!healthcenterLoading">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div
-            class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
-          >
-            <div
-              class="
-                shadow
-                overflow-hidden
-                border-b border-gray-200
-                sm:rounded-lg
-              "
-            >
-              <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
-                      "
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
-                      "
-                    >
-                      Email
-                    </th>
-                    <th
-                      scope="col"
-                      class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
-                      "
-                    >
-                      Region
-                    </th>
-                    <th
-                      scope="col"
-                      class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
-                      "
-                    >
-                      City
-                    </th>
-                    <th
-                      scope="col"
-                      class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
-                      "
-                    >
-                      Medical License
-                    </th>
-                    <th
-                      scope="col"
-                      class="
-                        px-6
-                        py-3
-                        text-left text-xs
-                        font-medium
-                        text-gray-500
-                        uppercase
-                        tracking-wider
-                      "
-                    >
-                      Actions
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
-                  <tr
-                    v-for="healthcenter in healthcenters.granted"
-                    :key="healthcenter._id"
-                  >
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm font-medium text-gray-900">
-                        {{ healthcenter.name }}
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm text-gray-900">
-                        {{ healthcenter.email }}
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm text-gray-900">
-                        {{ healthcenter.region }}
-                      </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <div class="text-sm text-gray-900">
-                        {{ healthcenter.city }}
-                      </div>
-                    </td>
-                    <td
-                      class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
-                    >
-                      {{ healthcenter.medicallicense }}
-                    </td>
-                    <td
-                      class="
-                        px-6
-                        py-4
-                        whitespace-nowrap
-                        text-right text-sm
-                        font-medium
-                      "
-                    >
-                      <button
-                        class="
-                          bg-red-500
-                          hover:bg-red-700
-                          text-white
-                          font-bold
-                          py-2
-                          px-4
-                          rounded
-                        "
-                        @click="deny(healthcenter._id)"
-                      >
-                        Deny
-                        <span v-if="actionLoader === healthcenter._id"> Loading... </span>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="mx-20 mb-10 -mt-20">
-    <div class="body">
-      <div class="flex mt-3 ml-3">
-        <ul class="flex border-b">
-          <li class="-mb-px mr-1">
-            <a
-              class="
-                bg-white
-                inline-block
-                border-l border-t border-r
-                rounded-t
-                py-2
-                px-4
-                text-blue-700
-                font-semibold
-              "
-              href="#"
               >Requested</a
             >
           </li>
@@ -375,7 +182,7 @@
                         @click="approve(healthcenter._id)"
                       >
                         Approve
-                        <span v-if="actionLoader === healthcenter._id"> Loading... </span>
+                        <span v-if="actionLoader === `${healthcenter._id}-approve`"> Loading... </span>
                       </button>
                       <button
                         class="
@@ -390,7 +197,7 @@
                         @click="deny(healthcenter._id)"
                       >
                         Deny
-                        <span v-if="actionLoader === healthcenter._id"> Loading... </span>
+                        <span v-if="actionLoader === `${healthcenter._id}-deny`"> Loading... </span>
                       </button>
                     </td>
                   </tr>
@@ -402,6 +209,200 @@
       </div>
     </div>
   </div>
+  <div class="mx-20 mb-10 -mt-20">
+    <div class="body">
+      <div class="flex mt-3 ml-3">
+        <ul class="flex border-b">
+          <li class="-mb-px mr-1">
+            <a
+              class="
+                bg-white
+                inline-block
+                border-l border-t border-r
+                rounded-t
+                py-2
+                px-4
+                text-blue-700
+                font-semibold
+              "
+              href="#"
+              >Approved</a
+            >
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="healthcenterLoading" class="flex flex-col justify-center items-center">
+        <Loader :loaderColor="loaderColor"/>
+      </div>
+
+      <div class="flex flex-col my-5" v-if="!healthcenterLoading">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div
+            class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
+          >
+            <div
+              class="
+                shadow
+                overflow-hidden
+                border-b border-gray-200
+                sm:rounded-lg
+              "
+            >
+              <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      Region
+                    </th>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      City
+                    </th>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      Medical License
+                    </th>
+                    <th
+                      scope="col"
+                      class="
+                        px-6
+                        py-3
+                        text-left text-xs
+                        font-medium
+                        text-gray-500
+                        uppercase
+                        tracking-wider
+                      "
+                    >
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                  <tr
+                    v-for="healthcenter in healthcenters.granted"
+                    :key="healthcenter._id"
+                  >
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ healthcenter.name }}
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">
+                        {{ healthcenter.email }}
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">
+                        {{ healthcenter.region }}
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">
+                        {{ healthcenter.city }}
+                      </div>
+                    </td>
+                    <td
+                      class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                    >
+                      {{ healthcenter.medicallicense }}
+                    </td>
+                    <td
+                      class="
+                        px-6
+                        py-4
+                        whitespace-nowrap
+                        text-right text-sm
+                        font-medium
+                      "
+                    >
+                      <button
+                        class="
+                          bg-red-500
+                          hover:bg-red-700
+                          text-white
+                          font-bold
+                          py-2
+                          px-4
+                          rounded
+                        "
+                        @click="deny(healthcenter._id)"
+                      >
+                        Deny
+                        <span v-if="actionLoader === `${healthcenter._id}-deny`"> Loading... </span>
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 </template>
 
 <script lang="ts">
@@ -449,7 +450,7 @@ export default defineComponent({
 
     const deny = async (id) => {
       // not implemented at the backend yet
-      // await store.dispatch("healthcenter/deleteHealthcenter", id, { root: true });
+      await store.dispatch("healthcenter/deleteHealthcenter", id, { root: true });
       // delete from healthcenters.requested & healthcenters.granted
       healthcenters.value.requested = healthcenters.value.requested.filter((hc) => hc._id !== id);
       healthcenters.value.granted = healthcenters.value.granted.filter((hc) => hc._id !== id);
